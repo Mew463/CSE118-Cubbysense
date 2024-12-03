f022b255f8620263e9980658e97b14aad2009f5a
Darell Chua <Darell27@gmail.com>
2024-12-03 14:33:34 -0800

    Update BLE Server (Brian)
    Update BLE Server (Brian)

--- Software/ble_server/main.py
+++ Software/ble_server/main.py
@@ -2,7 +2,7 @@ from bluetooth import *
 import requests
 
 import asyncio
-cubby = BLE_UART(peripheral_name='CubbySense', address = 'D381E5F7-AFC1-E128-7309-5C87C3123971')
+cubby = BLE_UART(peripheral_name='CubbySense', address = 'F0:F5:BD:51:E9:8D')
 
 host_ip = "192.168.0.141"
 host_port = "8081"
@@ -10,25 +10,30 @@ host_port = "8081"
 async def update_leds():
   await cubby.connect()
   while True:
-    x = input("msg:")
+    #x = input("msg:")
     if (cubby.isConnected):
       response = requests.get("{host_ip}:{host_port}/leds")
       # Check the status of the response
       if response.status_code == 200:  # HTTP 200 means OK
           print("Success!")
+          leds_data = response.json()
+          sorted_leds = sorted(leds_data["leds"], key=lambda led: led["id"])
+          message = ""
           for led in response["leds"]:
-            led[]
-          
-          
-          print(response.text)  # The response content as a string
+            if led["color"] == "off"
+              message += "0"
+            if led["color"] == "on"
+              message += "1"
+            if led["color"] == "new"
+              message += "2"
+          print(f"Formatted LED message: {message}")
       else:
           print(f"Request failed with status code: {response.status_code}")
-      
+
       asyncio.sleep(0.5)
-      
-      await cubby.write(x)
-    else:  
+
+      await cubby.write(message.encode())
+    else:
       await cubby.connect()
-  
 
 asyncio.run(update_leds())
